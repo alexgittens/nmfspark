410575100bf0eb5cf727d9d801a744c8d5898c62
Alex Gittens <gittens@icsi.berkeley.edu>
2016-04-13 16:48:02 -0700

    added test suite for xray algorithm, tidied up implementation of xray

--- src/main/scala/xray.scala
+++ src/main/scala/xray.scala
@@ -6,44 +6,33 @@ import math.sqrt
 
 object xray {
 
-/*
-  def main(args: Array[String] ) = {
-    val testMat = BDM.rand(20000, 200)
-    val selColIndices = computeXray(testMat, 20) 
-    println("Selected columns " + selColIndices.mkString(" "))
+  def findWeights(X : BDM[Double], XA : BDM[Double]) : BDM[Double] = {
+    val H = BDM.zeros[Double](XA.cols, X.cols)
+    val ws = NNLS.createWorkspace(XA.cols)
+    val ata = (XA.t*XA).toArray
+
+    for(colidx <- 0 until X.cols) {
+      val atb = (XA.t*X(::,colidx)).toArray
+      val h = NNLS.solve(ata, atb, ws) 
+      H(::, colidx) := BDV(h)
+    }
+    H
   }
-*/
 
   def computeXray( X : BDM[Double], r : Int) : (Array[Int], BDM[Double]) = {
 
     val C = X.t*X
-    val projOntoBasis = BDV.zeros[Double](r) // holds X^T_A * X_j
     val selColIndices = Array.fill[Int](r)(-1)
-    val H = BDM.zeros[Double](X.cols, r)
 
     def posPartNorm(x : BDV[Double]) : Double = {
       norm(x.map( x => if (x >= 0) x else 0 ))
-    }
-
-    // min ||X - XA*H||_F subject to H >= 0
-    def computeH(colindices: Array[Int], XA : BDM[Double]) = {
-      val H = BDM.zeros[Double](XA.cols, X.cols)
-      val ws = NNLS.createWorkspace(XA.cols)
-      val ata = (XA.t*XA).toArray
-
-      for(colidx <- 0 until r) {
-        val atb = (XA.t*X(::,colidx)).toArray
-        val h = NNLS.solve(ata, atb, ws) 
-        H(::, colidx) := BDV(h)
-      }
-      H
     }
 
     // select the first column
     val objVals = (0 until X.cols).map( j =>  posPartNorm(C(::, j)) / sqrt(C(j, j)) ).toList
     selColIndices(0) = objVals.indexOf(objVals.max) 
     var curXA = X(::, selColIndices(0)).asDenseMatrix.t
-    var curH = computeH(selColIndices, curXA)
+    var curH = findWeights(X, curXA)
 
     // select remaining columns
     for (curiter <- 1 until r) {
@@ -55,7 +44,7 @@ object xray {
       for(colidx <- 0 to curiter) {
         curXA(::, colidx) := X(::, selColIndices(colidx))
       }
-      curH = computeH(selColIndices, curXA)
+      curH = findWeights(X, curXA)
     }
 
     return (selColIndices, curH)
